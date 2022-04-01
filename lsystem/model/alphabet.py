d0c98a69fcd0ed91ec9d9234514df2719e3ac70a
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-04-01 13:06:53 +0200

    Added - Alphabet implementation.

--- lsystem/model/alphabet.py
+++ lsystem/model/alphabet.py
@@ -1,11 +1,76 @@
+from __future__ import annotations
+from typing import List
+
 import lsystem.model.symbol as symbol
 
 
 __all__ = ['Alphabet']
 
 
-class Alphabet(set):
-    
-    
-    def __init__(self) -> None:
-        super().__init__()
+class Alphabet:
+
+    def __init__(self, nonterminals: List[symbol.Symbol], terminals: List[symbol.Symbol]) -> None:
+
+        self.nonterminals = set(nonterminals)
+        self.terminals = set(terminals)
+
+        self.characters = self.nonterminals.union(self.terminals)
+
+    def is_nonterminal(self, key: symbol.Symbol) -> bool:
+        return key in self.nonterminals
+
+    def is_terminal(self, key: symbol.Symbol) -> bool:
+        return key in self.terminals
+
+    def __or__(self, other: object) -> Alphabet:
+        return self.union(other)
+
+    def union(self, other: Alphabet) -> Alphabet:
+        if not isinstance(other, self.__class__):
+            raise TypeError(
+                f'Invalid operand {other}. Union needs two operands of type {self.__module__}.Alphabet')
+
+        nt = self.nonterminals.union(other.nonterminals)
+        t = self.terminals.union(other.terminals)
+        return Alphabet(nt, t)
+
+    def __sub__(self, other: object) -> Alphabet:
+        return self.difference(other)
+
+    def difference(self, other: object) -> Alphabet:
+        if not isinstance(other, self.__class__):
+            raise TypeError(
+                f'Invalid operand {other}. Difference needs two operands of type {self.__module__}.Alphabet')
+
+        nt = self.nonterminals.difference(other.nonterminals)
+        t = self.terminals.difference(other.terminals)
+        return Alphabet(nt, t)
+
+    def __and__(self, other: object) -> Alphabet:
+        return self.intersection(other)
+
+    def intersection(self, other: object) -> Alphabet:
+        if not isinstance(other, self.__class__):
+            raise TypeError(
+                f'Invalid operand {other}. Intersection needs two operands of type {self.__module__}.Alphabet')
+
+        nt = self.nonterminals.intersection(other.nonterminals)
+        t = self.terminals.intersection(other.terminals)
+        return Alphabet(nt, t)
+
+    def __contains__(self, key: symbol.Symbol) -> bool:
+        return key in self.characters
+
+    def __repr__(self) -> str:
+        return f'<{self.__module__}.Alphabet({self.characters}), {hex(id(self))}>'
+
+    def __str__(self) -> str:
+        return str(self.characters)
+
+    def __eq__(self, __x: object) -> bool:
+        return (isinstance(__x, self.__class__)
+                and __x.nonterminals == self.nonterminals
+                and __x.terminals == self.terminals)
+
+    def __hash__(self) -> int:
+        return hash(self.value)
