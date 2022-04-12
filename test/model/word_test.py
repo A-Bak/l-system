f66de88cc52c3f4af52874bdd8a9c8f95354080f
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-04-12 11:44:02 +0200

    Test - extended testcases for Word (membership, iter, empty Word)

--- test/model/word_test.py
+++ test/model/word_test.py
@@ -16,15 +16,34 @@ class TestModelWord(unittest.TestCase):
 
     def test_constructor(self):
         """ Test if constructors raises TypeError fo invalid types. """
+        Word()
         self.assertRaises(TypeError, Word, [1, 2, 3, 4])
         self.assertRaises(TypeError, Word, 110011)
         self.assertRaises(TypeError, Word, 101.01)
 
     def test_equals(self):
         """ Test equality of words. """
+        self.assertEqual(Word(), Word())
         self.assertEqual(Word('ABAB'), self.w1)
         self.assertEqual(self.w1, self.w2)
         self.assertNotEqual(self.w1, self.w3)
+
+    def test_contains(self):
+        """ Test membership of symbols in words. """
+        self.assertFalse(Symbol('A') in Word())
+
+        self.assertTrue(Symbol('A') in self.w1)
+        self.assertTrue(Symbol('B') in self.w1)
+        self.assertFalse(Symbol('C') in self.w1)
+        self.assertFalse(Symbol('a') in self.w1)
+
+    def test_iter(self):
+        """ Test iterating over symbols of words. """
+        for char, symbol in zip('ABAB', self.w1):
+            self.assertEqual(char, str(symbol))
+
+        for s in Word():
+            raise ValueError('Empty word should not contain any symbols.')
 
     def test_representation(self):
         """ Test printable output of a word. """
