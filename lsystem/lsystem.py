9f7db8094fe2ef27b3aba166ba116a87ab5c94ec
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-04-22 15:51:24 +0200

    Added - LSystem JSON encoder, decoder and from_json(), to_json().

--- lsystem/lsystem.py
+++ lsystem/lsystem.py
@@ -1,43 +1,48 @@
 from __future__ import annotations
-from typing import Union
+from typing import TYPE_CHECKING, Union, Any
 
+if TYPE_CHECKING:
+    from lsystem.common import FilePath
+
+import json
 
 from lsystem.model.grammar import Grammar
 from lsystem.model.symbol import Symbol
 from lsystem.model.word import Word
 from lsystem.model.rule import Rule
 
-__all__ = ['LSystem']
+__all__ = ['LSystem', 'LSystemJSONEncoder', 'LSystemJSONDecoder']
 
 
 class LSystem():
 
-    def __init__(self) -> None:
+    def __init__(self, grammar: Grammar, instruction_mapping: Any, configuration: Any) -> None:
 
-        nt = [Symbol('F'), Symbol('G')]
-        t = [Symbol('+'), Symbol('-')]
-        rules = [
-            Rule(Symbol('F'), Word('F−G+F+G−F')),
-            Rule(Symbol('G'), Word('GG')),
-        ]
+        # nt = [Symbol('F'), Symbol('G')]
+        # t = [Symbol('+'), Symbol('-')]
+        # rules = [
+        #     Rule(Symbol('F'), Word('F+G')),
+        #     Rule(Symbol('G'), Word('F-G')),
+        # ]
+        # axiom = Word('F')
 
-        self.grammar = Grammar(nt, t, rules)
+        # self.grammar = Grammar(nt, t, rules, axiom)
+        # self.word = axiom
 
-        self.axiom = Word('F−G−G')
-        self.word = self.axiom
+        # self.instruction_mapping = {
+        #     'F': None,
+        #     '+': None,
+        #     '-': None,
+        #     '[': None,
+        #     ']': None,
+        # }
 
-        self.instructions = {
-            'F': None,
-            '+': None,
-            '-': None,
-            '[': None,
-            ']': None,
-        }
+        # self.angle = 90
+        # self.length = 7
 
-        self.angle = 90
-        self.length = 10
-
-        pass
+        self.grammar = grammar
+        self.instruction_mapping = instruction_mapping
+        self.configuration = configuration
 
     def next_derivation(self, w: Word) -> Union[Symbol, Word]:
 
@@ -48,3 +53,77 @@ class LSystem():
             yield derivation
 
         self.word = new_word
+
+    def to_json(self, path_to_file: FilePath = None) -> str:
+
+        json_string = json.dumps(self, cls=LSystemJSONEncoder)
+
+        if path_to_file is not None:
+            with open(path_to_file, 'w') as f:
+                f.write(json_string)
+
+        return json_string
+
+    @classmethod
+    def from_json(cls, s: str = None, path_to_file: FilePath = None) -> LSystem:
+
+        if s is not None:
+            return json.loads(s, cls=LSystemJSONDecoder)
+
+        elif path_to_file is not None:
+            with open(path_to_file, 'r') as f:
+                return json.load(f, cls=LSystemJSONDecoder)
+
+        else:
+            raise ValueError('''Failed to load LSystem from JSON,
+                             no JSON string or FilePath to JSON file
+                             were passed to LSystem.from_json().''')
+
+
+class LSystemJSONEncoder(json.JSONEncoder):
+
+    def default(self, o: LSystem) -> Any:
+
+        if not isinstance(o, LSystem):
+            raise TypeError(f'Invalid type {type(o)} for LSystemJSONEncoder.')
+
+        nt_list = [str(symbol) for symbol in o.grammar.alphabet.nonterminals]
+        t_list = [str(symbol) for symbol in o.grammar.alphabet.terminals]
+        rule_list = [[str(r.left_side), str(r.right_side)]
+                     for r in o.grammar.ruleset]
+        axiom = str(o.grammar.axiom)
+
+        lsystem_dict = {
+            'grammar': {
+                'nonterminals': nt_list,
+                'terminals': t_list,
+                'rules': rule_list,
+                'axiom': axiom
+            },
+            'mapping': o.instruction_mapping,
+            'config': o.configuration
+        }
+
+        return lsystem_dict
+
+
+class LSystemJSONDecoder(json.JSONDecoder):
+
+    def decode(self, s: str) -> LSystem:
+
+        json_dict = json.loads(s)
+
+        nonterminals = [Symbol(x)
+                        for x in json_dict['grammar']['nonterminals']]
+        terminals = [Symbol(x)
+                     for x in json_dict['grammar']['terminals']]
+        rules = [Rule(Symbol(l), Word(r))
+                 for l, r in json_dict['grammar']['rules']]
+        axiom = Word(json_dict['grammar']['axiom'])
+
+        grammar = Grammar(nonterminals, terminals, rules, axiom)
+
+        instruction_mapping = json_dict['mapping']
+        config = json_dict['config']
+
+        return LSystem(grammar, instruction_mapping, config)
---
+++ test/lsystem_test.py
@@ -0,0 +1,43 @@
+import unittest
+
+import os
+import json
+
+from lsystem import LSystem, LSystemJSONEncoder
+
+
+class TestModelSymbol(unittest.TestCase):
+    """ Test L-System. """
+
+    def setUp(self) -> None:
+        pass
+
+    def test_constructor(self):
+
+        # ls = LSystem()
+        pass
+
+    def test_json(self):
+
+        file_path = 'test/resources/default_lsystem.json'
+        self.assertTrue(os.path.exists(file_path))
+
+        with open(file_path, 'r') as f:
+            json_string = f.read()
+
+        ls1 = LSystem.from_json(path_to_file=file_path)
+        ls2 = LSystem.from_json(s=json_string)
+
+        self.assertIsNotNone(ls1)
+        self.assertIsNotNone(ls2)
+        self.assertEqual(ls1.grammar.alphabet, ls2.grammar.alphabet)
+        self.assertEqual(ls1.grammar.axiom, ls2.grammar.axiom)
+
+        for r1, r2 in zip(ls1.grammar.ruleset, ls2.grammar.ruleset):
+            self.assertEqual(r1, r2)
+
+        self.assertIsNotNone(ls1.to_json())
+
+
+if __name__ == "__main__":
+    unittest.main()
