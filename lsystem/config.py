9b84d86a68a3f3bec80f9d4cc39671f2549443e3
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-04-25 15:25:36 +0200

    Added - LSystemConfig and LSystemMapping classes.

---
+++ lsystem/config.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from typing import Mapping, overload
+
+
+__all__ = ['LSystemConfig']
+
+
+class LSystemConfig:
+
+    @overload
+    def __init__(self, angle_offset: int, segment_length: int) -> None:
+        ...
+
+    @overload
+    def __init__(self, dict: Mapping[str, int]) -> None:
+        ...
+
+    def __init__(self, *args) -> None:
+
+        if len(args) == 1 and isinstance(args[0], dict):
+            self.angle = args[0]['angle']
+            self.length = args[0]['length']
+
+        elif len(args) == 2 and isinstance(args[1], int) and isinstance(args[2], int):
+            self.angle = args[0]
+            self.length = args[1]
+
+        else:
+            raise NotImplementedError(
+                f'InvalidArguments: LSystemConfig constructor does not support given arguments "{list(map(type, args))}".')
+
+    def to_json(self) -> Mapping(str, int):
+        return {'angle': self.angle,
+                'length': self.length}
+
+    @classmethod
+    def from_json(cls, config_dict: Mapping[str, int]):
+        return LSystemConfig(config_dict)
---
+++ lsystem/mapping.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Any, Mapping
+
+from lsystem.model.symbol import Symbol
+from lsystem.common import Instruction
+
+
+__all__ = ['LSystemConfig']
+
+
+class LSystemMapping:
+
+    def __init__(self, instruction_mapping: Mapping[Symbol, Instruction]) -> None:
+        self.mapping = instruction_mapping
+
+    def to_json(self) -> Mapping[str, str]:
+        return {str(key): str(value) for key, value in self.mapping.items()}
+
+    @classmethod
+    def from_json(cls, str_dict: Mapping[str, str]) -> LSystemMapping:
+        instruction_mapping = {Symbol(key): Instruction.from_str(value)
+                               for key, value in str_dict.items()}
+        return LSystemMapping(instruction_mapping)
