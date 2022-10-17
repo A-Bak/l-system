18ccb2f52b46969045ea6a10bf4aa3987f337ad0
Adam Bak <59940328+A-Bak@users.noreply.github.com>
2022-10-17 16:01:04 +0200

    Fix - TurtleScreen.onkeypress() listener.

--- lsystem/scripts/gui.py
+++ lsystem/scripts/gui.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
-from lib2to3.pgen2 import grammar
-from typing import TYPE_CHECKING, Tuple
+from typing import TYPE_CHECKING, Callable, Protocol, Tuple
 
 if TYPE_CHECKING:
     from lsystem.model.word import Word
@@ -10,9 +9,8 @@ import turtle
 import hydra
 import hydra.utils
 from omegaconf import DictConfig
-from lsystem.config import LSystemConfig, RendererConfig, GUIWindowConfig
+from lsystem.config import LSystemConfig
 
-from lsystem.model.grammar import Grammar
 from lsystem.derivator import GrammarDerivator
 from lsystem.renderer import TurtleRenderer
 
@@ -23,52 +21,68 @@ __all__ = ["LSystemGUI"]
 HYDRA_CONFIG_PATH = "../../conf"
 
 
+def create_turtle_screen(
+    title: str,
+    window_size: Tuple[int, int],
+    line_color: Tuple[int, int, int],
+    background_color: Tuple[int, int, int],
+) -> turtle.TurtleScreen:
+
+    turtle.hideturtle()
+    turtle.degrees(360)
+    turtle.colormode(255)
+    turtle.pencolor(line_color)
+    turtle.tracer(2, 100)
+
+    screen = turtle.Screen()
+    screen.title(title)
+    screen.setup(*window_size)
+    screen.colormode(255)
+    screen.bgcolor(background_color)
+
+    return screen
+
+
+class LSystemWindow(Protocol):
+    def onscreenclick(self, function: Callable) -> None:
+        """Perform action on the canvas when clicked."""
+
+    def onkeypress(self, function: Callable, key_symbol: str) -> None:
+        """Perform action when the key_symbol key is pressed."""
+
+    def update(self) -> None:
+        """Perform update on the canvas."""
+
+
 class LSystemGUI:
     def __init__(
         self,
-        title: str,
-        gui_config: GUIWindowConfig,
+        screen: LSystemWindow,
         derivator: GrammarDerivator,
         renderer: TurtleRenderer,
     ) -> None:
 
-        self.screen = self._create_turtle_screen(
-            title,
-            gui_config.window_size,
-            gui_config.line_color,
-            gui_config.background_color,
-        )
-
+        self.screen = screen
         self.derivator = derivator
         self.renderer = renderer
 
         self.display_word(self.derivator.word)
-
-    def _create_turtle_screen(
-        self,
-        title: str,
-        window_size: Tuple[int, int],
-        line_color: Tuple[int, int, int],
-        background_color: Tuple[int, int, int],
-    ) -> turtle.TurtleScreen:
-
-        turtle.hideturtle()
-        turtle.degrees(360)
-        turtle.colormode(255)
-        turtle.pencolor(line_color)
-        turtle.tracer(2, 100)
-
-        screen = turtle.Screen()
-        screen.title(title)
-        screen.setup(*window_size)
-        screen.colormode(255)
-        screen.bgcolor(background_color)
-        screen.onscreenclick(self._on_click)
-
-        return screen
+        self.allow_actions()
 
     def _on_click(self, pos_x: float, pos_y: float) -> None:
         self.display_word()
+
+    def _on_key_press(self) -> None:
+        self.display_word()
+
+    def stop_actions(self) -> None:
+        self.screen.onscreenclick(None)
+        self.screen.onkeypress(None, "space")
+
+    def allow_actions(self) -> None:
+        self.screen.onscreenclick(self._on_click)
+        turtle.listen()
+        self.screen.onkeypress(self._on_key_press, key="space")
 
     def display_word(self, word: Word = None) -> None:
 
@@ -78,9 +92,9 @@ class LSystemGUI:
             else self.derivator.next_derivation_gen(self.derivator.word)
         )
 
-        self.screen.onscreenclick(None)
+        self.stop_actions()
         self.renderer.draw(element=displayed_word, screen=self.screen)
-        self.screen.onscreenclick(self._on_click)
+        self.allow_actions()
 
         if word is not None:
             print(f"{word}\n")
@@ -98,10 +112,10 @@ def main(config: DictConfig):
 
     derivator = GrammarDerivator(lsystem_cfg.grammar)
     renderer = TurtleRenderer(lsystem_cfg.renderer_config)
+    screen = create_turtle_screen(title=lsystem_cfg.name, **gui_cfg)
 
     gui = LSystemGUI(
-        title=lsystem_cfg.name,
-        gui_config=gui_cfg,
+        screen=screen,
         derivator=derivator,
         renderer=renderer,
     )
